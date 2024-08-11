8abf031985f99845bb6d24f7233d001834166475
hamed gandomkar <dev.hamedgk@yahoo.com>
2024-08-12 01:52:59 +0330

    two more tests

--- xml_parser_test.go
+++ xml_parser_test.go
@@ -20,6 +20,53 @@ var extractTable = []struct {
 			},
 		},
 	},
+	{
+		query: "item{title,guid,description,dc:creator}",
+		expected: []*TagData{
+			{
+				TagName: "title",
+				Content: "Hamas Elevates Gaza Leader Who Planned Oct. 7 Attacks to Top Post",
+			},
+			{
+				TagName: "guid",
+				Content: "https://www.nytimes.com/2024/08/06/world/middleeast/hamas-yahya-sinwar-promoted.html",
+			},
+			{
+				TagName: "description",
+				Content: "The selection of Yahya Sinwar, a prime target of Israeli forces, to replace the assassinated Ismail Haniyeh consolidates authority in the hands of a hard-liner who is in hiding.",
+			},
+			{
+				TagName: "dc:creator",
+				Content: "Adam Rasgon, Aaron Boxerman, Euan Ward and Michael Levenson",
+			},
+		},
+	},
+	{
+		query: "item{title,guid,description,category[domain],media:content[url]}",
+		expected: []*TagData{
+			{
+				TagName: "title",
+				Content: "Hamas Elevates Gaza Leader Who Planned Oct. 7 Attacks to Top Post",
+			},
+			{
+				TagName: "guid",
+				Content: "https://www.nytimes.com/2024/08/06/world/middleeast/hamas-yahya-sinwar-promoted.html",
+			},
+			{
+				TagName: "description",
+				Content: "The selection of Yahya Sinwar, a prime target of Israeli forces, to replace the assassinated Ismail Haniyeh consolidates authority in the hands of a hard-liner who is in hiding.",
+			},
+			{
+				TagName:    "category",
+				Content:    "Israel-Gaza War (2023- )",
+				Attributes: map[string]string{"domain": "http://www.nytimes.com/namespaces/keywords/des"},
+			},
+			{
+				TagName:    "media:content",
+				Attributes: map[string]string{"url": "https://static01.nyt.com/images/2024/08/06/multimedia/06mideast-crisis-sinwar-photo-plwh/06mideast-crisis-sinwar-photo-plwh-mediumSquareAt3X-v2.jpg"},
+			},
+		},
+	},
 }
 
 func TestXMLParser(t *testing.T) {
@@ -39,7 +86,7 @@ func runTest(t *testing.T, text string) {
 		tokens := ParseRSSQuery(c.query)
 		output, _ := Extract(text, tokens, 0)
 		if !reflect.DeepEqual(c.expected, output) {
-			t.Log(c.query)
+			t.Log("failed extracting", c.query)
 			t.Fail()
 		}
 	}
